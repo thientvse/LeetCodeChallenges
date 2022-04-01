85ede0e4f03246dd98f261e914dbd66ace27a9fb
Thientvse <thientvse@gmail.com>
2022-04-01 14:44:35 +0700

    Update 169-majority-element.java
    Update more way to resolve problem

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -15,4 +15,21 @@ class Solution {
                     .filter(entry -> entry.getValue() == Collections.max(hashMap.values()))
                     .map(Map.Entry::getKey).findAny().orElse(0);
     }
+    
+    // new way
+    public static int majorityElement(int[] nums){
+            int count = 0;
+            Integer candidate = null;
+
+            for (int num: nums) {
+                if (count == 0){
+                    candidate = num;
+                }
+                count += (num == candidate) ? 1:1;
+            }
+
+            return candidate;
+        }
 }
+
+

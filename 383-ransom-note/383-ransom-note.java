8b2a00b375f138c54e73b96ecf8e85bfdbb9e128
Thientvse <thientvse@gmail.com>
2022-08-22 16:57:04 +0700

    Time: 40 ms (18.20%), Space: 52.4 MB (24.71%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for (char c: magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        
+        for (char c: ransomNote.toCharArray()){
+            if (map.containsKey(c)){
+                map.put(c,map.get(c)-1);
+                if (map.get(c)==0){
+                    map.remove(c);
+                }
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+}

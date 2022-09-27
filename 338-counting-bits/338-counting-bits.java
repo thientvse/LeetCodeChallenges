37c20c9f519fa176a2eee4696e91487cdaa129af
Thientvse <thientvse@gmail.com>
2022-09-27 10:15:36 +0700

    Time: 16 ms (14.38%), Space: 48.2 MB (48.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,14 +1,27 @@
 class Solution {
     public int[] countBits(int n) {
+        
+        if(n == 0)    return new int[] {0};
+        if(n == 1)    return new int[] {0,1};
+        
         int[] dp = new int[n+1];
         
         dp[0] = 0;
+        dp[1] = 1;
         
-        for(int i = 1; i <= n; i++){
-            if(i%2 == 0)
-                dp[i] = dp[i/2];
-            else
-                dp[i] = 1 + dp[i-1];
+        int lastPower = 1;
+        
+        for(int i = 2; i <= n; i++){
+           // if i is power, it is only value 1
+            
+            if (i == 2*lastPower){
+                lastPower *= 2; // update
+                dp[i] = 1;
+            } else {
+                dp[i] = dp[lastPower] + dp[i-lastPower];
+            }
+            
+            
         }
         
         return dp;

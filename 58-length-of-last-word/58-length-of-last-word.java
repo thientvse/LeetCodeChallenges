7a58e097ab4da42544156ca72d502cc814a56c3e
Thientvse <thientvse@gmail.com>
2022-09-30 14:41:19 +0700

    Time: 0 ms (100.00%), Space: 42.5 MB (21.69%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,7 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
+//         String[] words = s.split(" ");
         
-        return words[words.length-1].length();
+//         return words[words.length-1].length();
+        
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--){
+            if (s.charAt(i) != ' '){
+                length++;
+            } else {
+                if (length > 0) return length;
+            }
+        }
+        
+        return length;
+        
     }
 }

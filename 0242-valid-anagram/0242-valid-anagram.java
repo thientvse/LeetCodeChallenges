201079c5c2a8f7dca33c4152661b8e316a794250
Thientvse <thientvse@gmail.com>
2022-10-25 14:34:30 +0700

    Time: 76 ms (5.07%), Space: 43.5 MB (65.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        if(s.length() != t.length()) return false;
+        
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)-1);
+        }
+        
+        for(char c: map.keySet()){
+            if(map.get(c)!=0) return false;
+        }
+        
+        return true;
+        
+        /** 
+            1. tao mot hash map
+            2. duyet mang 
+               + add cac ky tu cua chuoi s vao hash map cung voi so lan xuat hienj
+               + remove cac ky tu cua chuoi t trong hashmap
+            3. duyet hash map
+               Neu con gia tri thi la false
+               Va nguoc lai
+        */ 
+        
+    }
+}

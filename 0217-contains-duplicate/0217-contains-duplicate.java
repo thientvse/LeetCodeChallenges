59b6f1fab9986c05f98bc89cae174faf9e4fec4c
Thientvse <thientvse@gmail.com>
2022-11-01 15:38:31 +0700

    Time: 30 ms (27.73%), Space: 70.5 MB (31.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> hp = new HashMap<>();
+        
+        for(int i=0; i < nums.length; i++){
+            if(hp.containsKey(nums[i])){
+                return true;
+            } 
+            hp.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}

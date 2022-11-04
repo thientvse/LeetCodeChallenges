6b063316371f7d17d49d3bd4bfadcfc4051b5507
Thientvse <thientvse@gmail.com>
2022-11-04 11:21:33 +0700

    Time: 1 ms (100.00%), Space: 39.3 MB (95.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,43 +1,43 @@
 class Solution {
     public int mySqrt(int x) {
-//         if (x <= 1) return x;
+        if (x <= 1) return x;
         
-//         int start  = 1;
-//         int end = x/2;
-        
-//         while (start <= end){
-//             int mid = start + (end - start)/2;
-            
-//             int div = x/mid;
-            
-//             if (div == mid){
-//                 return mid;
-//             }
-            
-//             if (div > mid){
-//                 start = mid + 1;
-//             }
-            
-//             if (div < mid){
-//                 end = mid - 1;
-//             }
-//         }
-        
-//         return start;
-        
-        if(x <= 1) return x;
-        int start = 1;
+        int start  = 1;
         int end = x/2;
         
-        while(start < end) {
-            int mid = (start + (end-start)/2) + 1;
+        while (start < end){
+            int mid = (start + (end - start)/2) + 1 ;
             
             int div = x/mid;
-            if(div == mid) return mid;
-            if(div > mid) start = mid;
-            else end = mid-1;
+            
+            if (div == mid){
+                return mid;
+            }
+            
+            if (div > mid){
+                start = mid;
+            }
+            
+            if (div < mid){
+                end = mid - 1;
+            }
         }
         
         return start;
+        
+//         if(x <= 1) return x;
+//         int start = 1;
+//         int end = x/2;
+        
+//         while(start < end) {
+//             int mid = (start + (end-start)/2) + 1;
+            
+//             int div = x/mid;
+//             if(div == mid) return mid;
+//             if(div > mid) start = mid;
+//             else end = mid-1;
+//         }
+        
+//         return start;
     }
 }

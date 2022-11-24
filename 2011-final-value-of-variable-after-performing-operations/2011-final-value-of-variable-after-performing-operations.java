05f47cbbca5104807b6cc904b15bf377a72d7ea2
Thientvse <thientvse@gmail.com>
2022-11-24 15:10:01 +0700

    Time: 0 ms (100.00%), Space: 42.1 MB (83.86%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -3,12 +3,20 @@ class Solution {
         
         int rs = 0;
         
-        for (int i = 0; i < operations.length; i++){
-            if (operations[i].equals("--X")||operations[i].equals("X--")){
-                rs -= 1;
-            } else {
-                rs += 1;
-            }
+//         for (int i = 0; i < operations.length; i++){
+//             if (operations[i].equals("--X")||operations[i].equals("X--")){
+//                 rs -= 1;
+//             } else {
+//                 rs += 1;
+//             }
+//         }
+        
+//         return rs;
+        
+        for (String c: operations){
+            
+            if (c.charAt(1) == '-') rs--;
+            else rs++;
         }
         
         return rs;
